db4801b4b8f0e77f325ee35df75e86045ce435d6
Aleksandr Timokhin <adtimokhin@Aleksandrs-MacBook-Pro.local>
2024-07-22 23:56:29 +0500

    Added Cuberto Cursor

---
+++ src/scripts/cursor.js
@@ -0,0 +1,21 @@
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
+import "../styles/cursor.scss"
+
+MouseFollower.registerGSAP(gsap);
+
+const cursor = new MouseFollower({
+    speed: 0.8,
+    skewing: 1,
+    skewingText: 3
+});
+
+const links = gsap.utils.toArray("a");
+links.forEach((link)=>{
+    link.addEventListener("mouseenter", ()=>{
+        cursor.setText('explore');
+    });
+    link.addEventListener("mouseleave", ()=>{
+        cursor.removeText();
+    })
+})
